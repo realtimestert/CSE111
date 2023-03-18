f0067e24ab70d06dd7ff0f15b53d9d08afe2e5b6
realtimestert <istewpotter1@gmail.com>
2023-03-17 22:49:11 -0400

    Added project files

---
+++ pokemon_type_match.py
@@ -0,0 +1,47 @@
+import csv
+
+def main():
+
+    pokemon_list = read_compound_list("pokemon.csv")
+    print_list(pokemon_list)
+
+
+def read_compound_list(filename):
+    """Read the text from a CSV file into a compound list.
+    The compound list will contain small lists. Each small
+    list will contain the data from one row of the CSV file.
+
+    Parameter
+        filename: the name of the CSV file to read.
+    Return: the compound list
+    """
+    # Create an empty list.
+    compound_list = []
+
+    # Open the CSV file for reading.
+    with open(filename, "rt") as csv_file:
+
+        # Use the csv module to create a reader
+        # object that will read from the opened file.
+        reader = csv.reader(csv_file)
+
+        # The first line of the CSV file contains column headings
+        # and not a student's I-Number and name, so this statement
+        # skips the first line of the CSV file.
+        next(reader)
+
+        # Process each row in the CSV file.
+        for row in reader:
+
+            # Append the current row at the end of the compound list.
+            compound_list.append(row)
+
+    return compound_list
+
+def print_list(compound_list):
+    for element in compound_list:
+        print(element)
+    print()
+
+if __name__ == "__main__":
+    main()
