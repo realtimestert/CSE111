b20fcb1f4d4544736d88c1f39560d511031241f1
realtimestert <istewpotter1@gmail.com>
2023-03-20 22:37:15 -0400

    Changes in syntax

--- staff_of_chaos.py
+++ staff_of_chaos.py
@@ -1,13 +1,15 @@
 import random
 import csv
-import pytest
 from guizero import App, Text, PushButton
 
 def main():
-    #choice = input("Will you use the staff of Chaos? Y/N? ")
 
-    random_magic = read_compound_list("10000_random.csv")
+    MAGIC_INDEX = 0
+    RANDOM_MAGIC = 1
     
+    magic_dict = read_dictionary("10000_random.csv", MAGIC_INDEX)
+    
+    print_list(magic_dict)
     #Prints out a list of all the magical effects.
     #print_list(random_magic)
     app_window()
@@ -16,15 +18,19 @@ def app_window():
     
     # Nested Function
     def change_message():
-        message.value = rand_number(4)        
+        message.value = rand_number(4)  
+
+    def change_message_2():
+        message.value = "Okay"      
     
     app = App(title="Staff of Chaos")
-    message = Text(app, text = "Will you use the staff of chaos?")
+    message = Text(app, text = "Will you use the Staff of Chaos?")
     
     # Push the button to change the message
-    button = PushButton(app, text = "Do it", command = change_message)
-    app.display()
+    button1 = PushButton(app, align = "left", text = "Do it", command = change_message)
+    button2 = PushButton(app, align = "right", text = "Don't", command = change_message_2)
     
+    app.display()
 
 def rand_number(length, number=None):
     if number == None:
@@ -68,6 +74,27 @@ def read_compound_list(filename):
 
     return compound_list
 
+def read_dictionary(filename, key_column_index):
+    """Read the contents of a CSV file into a compound
+    dictionary and return the dictionary.
+    Parameters
+        filename: the name of the CSV file to read.
+        key_column_index: the index of the column
+            to use as the keys in the dictionary.
+    Return: a compound dictionary that contains
+        the contents of the CSV file.
+    """
+    dictionary = {}
+    
+    #Opening CSV file
+    with open (filename, "rt") as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)
+        for row_list in reader:
+            key = row_list[key_column_index]
+            dictionary[key] = row_list
+
+    return dictionary
 
 def print_list(compound_list):
     for element in compound_list:
