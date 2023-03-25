45f82418dd5f8820a2d8ca133259f7e5abfc892a
realtimestert <istewpotter1@gmail.com>
2023-03-24 21:11:36 -0400

    test program

--- test_staff.py
+++ test_staff.py
@@ -1,1 +1,11 @@
+
+from staff_of_chaos import random_magic
+from inspect import signature
+from os import path
+from tempfile import mktemp
 import pytest
+
+def test_random_magic():
+    
+    filename = mktemp(dir=".", prefix="not", suffix=".txt")
+
